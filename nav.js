db9720d28cef40f87a7b405dc6891a70ed46b6bc
MoffKalast <vid.rijavec@gmail.com>
2026-07-07 12:32:56 +0200

    label icons

--- nav.js
+++ nav.js
@@ -12,49 +12,49 @@ const NAV = [
 	{
 		group: "/general_tools",
 		pages: [
-			{ id: "general-tools/global-settings", title: "Global Settings" },
-			{ id: "general-tools/rosbridge", title: "Rosbridge" },
-			{ id: "general-tools/grid", title: "Grid" },
-			{ id: "general-tools/tf", title: "TF" },
-			{ id: "general-tools/robot-model", title: "Robot Model" },
-			{ id: "general-tools/param-reconfigure", title: "Param Reconfigure" },
-			{ id: "general-tools/bag-recorder", title: "Bag Recorder" },
-			{ id: "general-tools/node-manager", title: "Node Manager" },
-			{ id: "general-tools/topic-inspector", title: "Topic Inspector" },
-			{ id: "general-tools/add-widget", title: "Add Widget" }
+			{ id: "general-tools/global-settings", title: "Global Settings", icon: "settings.svg" },
+			{ id: "general-tools/rosbridge", title: "Rosbridge", icon: "rosbridge_connected.svg" },
+			{ id: "general-tools/grid", title: "Grid", icon: "grid.svg" },
+			{ id: "general-tools/tf", title: "TF", icon: "tf.svg" },
+			{ id: "general-tools/robot-model", title: "Robot Model", icon: "robotmodel.svg" },
+			{ id: "general-tools/param-reconfigure", title: "Param Reconfigure", icon: "reconfigure.svg" },
+			{ id: "general-tools/bag-recorder", title: "Bag Recorder", icon: "rosbag.svg" },
+			{ id: "general-tools/node-manager", title: "Node Manager", icon: "nodemgr.svg" },
+			{ id: "general-tools/topic-inspector", title: "Topic Inspector", icon: "inspector.svg" },
+			{ id: "general-tools/add-widget", title: "Add Widget", icon: "add.svg" }
 		]
 	},
 	{
 		group: "/mission_planning",
 		pages: [
-			{ id: "mission-planning/teleop-joystick", title: "Teleop Joystick" },
-			{ id: "mission-planning/pose-estimate", title: "2D Pose Estimate" },
-			{ id: "mission-planning/nav-goal", title: "2D Nav Goal" },
-			{ id: "mission-planning/waypoints", title: "Waypoint Mission" },
-			{ id: "mission-planning/area-mission", title: "Area Mission" },
-			{ id: "mission-planning/button", title: "Button" },
-			{ id: "mission-planning/altimeter", title: "Altimeter" }
+			{ id: "mission-planning/teleop-joystick", title: "Teleop Joystick", icon: "joystick.svg" },
+			{ id: "mission-planning/pose-estimate", title: "2D Pose Estimate", icon: "initialpose.svg" },
+			{ id: "mission-planning/nav-goal", title: "2D Nav Goal", icon: "simplegoal.svg" },
+			{ id: "mission-planning/waypoints", title: "Waypoint Mission", icon: "waypoints.svg" },
+			{ id: "mission-planning/area-mission", title: "Area Mission", icon: "area.svg" },
+			{ id: "mission-planning/button", title: "Button", icon: "button.svg" },
+			{ id: "mission-planning/altimeter", title: "Altimeter", icon: "altimeter_green.svg" }
 		]
 	},
 	{
 		group: "/data_visualization",
 		pages: [
-			{ id: "data-visualization/attitude-indicator", title: "Attitude Indicator" },
-			{ id: "data-visualization/pose-tracker", title: "Pose Tracker" },
-			{ id: "data-visualization/map", title: "Map" },
-			{ id: "data-visualization/satellite-tiles", title: "Satellite Tiles" },
-			{ id: "data-visualization/gridcells", title: "GridCells" },
-			{ id: "data-visualization/battery", title: "Battery" },
-			{ id: "data-visualization/compressed-image", title: "Compressed Image" },
-			{ id: "data-visualization/marker-array", title: "Marker Array" },
-			{ id: "data-visualization/path", title: "Path" },
-			{ id: "data-visualization/range", title: "Range" },
-			{ id: "data-visualization/laser-scan", title: "Laser Scan" },
-			{ id: "data-visualization/point-cloud", title: "Point Cloud" },
-			{ id: "data-visualization/pose-with-covariance", title: "Pose with Covariance" },
-			{ id: "data-visualization/pose-array", title: "Pose Array" },
-			{ id: "data-visualization/temperature", title: "Temperature" },
-			{ id: "data-visualization/speedometer", title: "Speedometer" }
+			{ id: "data-visualization/attitude-indicator", title: "Attitude Indicator", icon: "navball_icon.svg" },
+			{ id: "data-visualization/pose-tracker", title: "Pose Tracker", icon: "odom.svg" },
+			{ id: "data-visualization/map", title: "Map", icon: "map.svg" },
+			{ id: "data-visualization/satellite-tiles", title: "Satellite Tiles", icon: "satelite.svg" },
+			{ id: "data-visualization/gridcells", title: "GridCells", icon: "gridcells.svg" },
+			{ id: "data-visualization/battery", title: "Battery", icon: "battery_100.svg" },
+			{ id: "data-visualization/compressed-image", title: "Compressed Image", icon: "image.svg" },
+			{ id: "data-visualization/marker-array", title: "Marker Array", icon: "markerarray.svg" },
+			{ id: "data-visualization/path", title: "Path", icon: "path.svg" },
+			{ id: "data-visualization/range", title: "Range", icon: "range.svg" },
+			{ id: "data-visualization/laser-scan", title: "Laser Scan", icon: "scan.svg" },
+			{ id: "data-visualization/point-cloud", title: "Point Cloud", icon: "pointcloud.svg" },
+			{ id: "data-visualization/pose-with-covariance", title: "Pose with Covariance", icon: "posewithcovariancestamped.svg" },
+			{ id: "data-visualization/pose-array", title: "Pose Array", icon: "posearray.svg" },
+			{ id: "data-visualization/temperature", title: "Temperature", icon: "temp_warm.svg" },
+			{ id: "data-visualization/speedometer", title: "Speedometer", icon: "speedometer_icon.svg" }
 		]
 	}
 ];
